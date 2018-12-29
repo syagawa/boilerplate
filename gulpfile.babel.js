0b6a268d8e8d5d1386a1674d65e82e118ad75357
syagawa <yagawa@qzc.co.jp>
2018-12-29 15:06:04 +0900

    First commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,104 @@
+import gulp from 'gulp';
+
+import webpackStream from "webpack-stream";
+import webpack from "webpack";
+import webpackConfig from "./webpack.config";
+
+const notifier = require("node-notifier");
+
+import browser from "browser-sync";
+import plumber from "gulp-plumber";
+
+import autoprefixer from "gulp-autoprefixer";
+
+import pug from "gulp-pug";
+
+
+const paths = {
+  styles: {
+    src: 'src/styles/**/*.scss',
+    dest: 'dist/css/'
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: 'dist/js/'
+  },
+  pugs: {
+    src: 'src/pug/**/*.pug',
+    exclude: '!src/pug/**/_*.pug',
+    dest: 'dist/'
+  }
+};
+
+function errorHandler(error) {
+  var message;
+  if(error.message){
+    message =  error.message;
+  }else{
+    message = error;
+  }
+  notifier.notify({
+    title: 'Error occurred in gulp or Webpack processing.',
+    wait: true,
+    timeout: 30,
+    message: message,
+  }, function () {
+    console.log(error);
+  });
+}
+
+function webpackJSError(){
+  errorHandler("Webpack Error : JS");
+  this.emit("end");
+}
+function webpackCSSError(){
+  errorHandler("Webpack Error : CSS / SCSS");
+  this.emit("end");
+};
+
+function pugs(){
+  return gulp.src([paths.pugs.src, paths.pugs.exclude])
+    .pipe(pug())
+    .pipe(gulp.dest(paths.pugs.dest));
+}
+
+function js2js(){
+  return webpackStream(webpackConfig.js, webpack)
+    .on('error', webpackJSError)
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browser.reload({stream:true}));
+}
+
+function sass2css(){
+  return webpackStream(webpackConfig.css, webpack)
+    .on("error", webpackCSSError)
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browser.reload({stream:true}));
+}
+
+
+
+function watch(){
+  gulp.watch(paths.pugs.src, pugs);
+  gulp.watch(paths.scripts.src, js2js);
+  gulp.watch(paths.styles.src, sass2css);
+}
+
+function server(){
+  return browser({
+    server:{
+      baseDir: "./dist/",
+      index: "index.html"
+    },
+    port: 9000,
+    browser: "chrome"
+  });
+}
+
+const build = gulp.series(pugs, js2js, sass2css);
+gulp.task('build', build);
+
+const start = gulp.parallel( build, server, watch);
+gulp.task("start", start);
+
+export default start;
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const WebpackAutoInject = require("webpack-auto-inject-version");
+const path = require("path");
+
+let ENV = process.env.NODE_ENV;
+const MODE = process.env.APP_MODE;
+let settings_override_file_name = "js/settings/settings.empty.js";
+if(ENV !== "production"){
+  ENV = "development";
+  settings_override_file_name = "js/settings/settings.dev.js";
+}
+if(MODE === "test"){
+  settings_override_file_name = "js/settings/settings.test.js";
+}
+
+module.exports = {
+  "js": {
+    mode: ENV,
+    entry: {
+      "bundle": "./src/js/main.js"
+    },
+    output: {
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /execute\.js$/,
+          use: 'raw-loader'
+        }
+      ]
+    },
+    plugins: [
+      new WebpackAutoInject({
+        PACKAGE_JSON_PATH: 'package.json',
+        components: {
+          InjectAsComment: true
+        },
+        componentsOptions: {
+          InjectAsComment: {
+            tag: 'BOG Build Version {version} - {date}'
+          }
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.js',
+        // aliasxxx$: path.resolve(__dirname, 'js/test2.js')
+        settings_override$: path.resolve(__dirname, settings_override_file_name)
+      }
+    }
+  }
+  ,
+  "css": {
+    mode: ENV,
+    entry: {
+      "bundle": [
+        "./src/styles/main.scss"
+      ]
+    },
+    output: {
+      filename: "[name].css"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader?-url&minimize!sass-loader"
+          })
+        },
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader?-url&minimize"
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin("[name].css"),
+      new WebpackAutoInject({
+        PACKAGE_JSON_PATH: 'package.json',
+        components: {
+          InjectAsComment: true
+        },
+        componentsOptions: {
+          InjectAsComment: {
+            tag: 'BOG Build Version {version}'
+          }
+        }
+      })
+    ]
+  }
+
+};
